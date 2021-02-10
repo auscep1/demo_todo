d34959131c5eb4b0133e990ab127ae24987c2b1c
ausra.cepulionyte@gmail.com <Museliukas1>
2021-02-10 23:50:48 +0200

    first

---
+++ src/backend/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /** docker-compose exec backend php artisan migrate
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+    }
+}
